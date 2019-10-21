1e406e3331cc1fde7e8806057e04b62b3354e3e7
Xueming Li <xuemingl@mellanox.com>
2019-10-21 09:44:16 +0800

    main: add config pannel framework
    Signed-off-by: Xueming Li <xuemingl@mellanox.com>

--- panel/PanelConfig.py
+++ panel/PanelConfig.py
@@ -2,23 +2,73 @@ from scapy.all import *
 from flexx import flx
 
 
+class ConfigItem(flx.PyWidget):
+	def init(self, text):
+		self.label = flx.Label(text = text, flex=1)
+
+	@flx.reaction('label.pointer_click')
+	def on_click(self, *events):
+		self.root.pnl_config.on_config_load(self.label.text)
+
 class PanelConfig(flx.PyWidget):
 	def init(self):
 		with flx.VBox():
-			with flx.VBox(flex=1) as self.lst_config:
-				self.pnl_tx = flx.Label(text='ether')
-				self.pnl_tx = flx.Label(text='ipv4')
-				self.pnl_tx = flx.Label(text='ipv6')
-				self.pnl_tx = flx.Label(text='vxlan')
-				self.pnl_tx = flx.Label(text='vxlan-ali')
+			flx.Label(text = "Configurations:")
+			with flx.VBox() as self.lst_config:
+				self.pnl_tx = ConfigItem('ether')
+				self.pnl_tx = ConfigItem('ipv4')
+				self.pnl_tx = ConfigItem('ipv6')
+				self.pnl_tx = ConfigItem('vxlan')
+				self.pnl_tx = ConfigItem('vxlan-ali')
+			flx.VBox(flex=1)
 			self.btn_load = flx.Button(text='Load...')
 			self.btn_sniff = flx.Button(text='Sniff...')
 
+	# @flx.reaction('lst_config.children*.pointer_click')
+	# @flx.action
+	def on_config_load(self, name):
+		config = self.load_config(name)
+		pkt = self.parse_scapy(config)
+		if pkt != None:
+			self.root.show_tx(name, pkt) 
+
 	@flx.reaction('btn_load.pointer_click')
-	def on_load(self, *events):
+	def on_pcap_load(self, *events):
 		self.root.show_rx()
 
 	@flx.reaction('btn_sniff.pointer_click')
 	def on_sniff(self, *events):
 		self.root.show_rx()
 
+	def load_config(self, name):
+		# TODO
+		pkt = Ether() # /IP("1.2.3.4")/UDP(sport=123)
+		print(pkt)
+		config = self.scapy_dump(pkt)
+		print(config)
+		return config
+
+	def save_config(self, name, pkt):
+		config = scap_dump(pkt)
+		print(config)
+		# TODO
+		
+	def scapy_dump(self, pkt):
+		list = []
+		while not isinstance(pkt, NoPayload):
+			list.append([pkt.__class__.__name__, repr(pkt.fields)])
+			pkt = pkt.payload
+		return repr(list)
+	
+	def parse_scapy(self, str):
+		list = eval(str)
+		pkt = None
+		for layer_config in list:
+			layer = eval(layer_config[0]+"()")
+			fields = eval(layer_config[1])
+			layer.fields = fields
+			if pkt == None:
+				pkt = layer
+			else:
+				pkt.add_payload(layer)
+		return pkt
--- scapy-ui.py
+++ scapy-ui.py
@@ -9,20 +9,27 @@ from panel.PanelConfig import *
 class ScapyUI(flx.PyWidget):
 	def init(self):
 		with flx.VBox():
-			with flx.HSplit() as self.pnl_main:
-				self.pnl_config = PanelConfig()
-				with flx.VBox():
+			with flx.HSplit(flex=1) as self.pnl_main:
+				self.pnl_config = PanelConfig(flex=1)
+				with flx.VBox(flex=6):
 					self.pnl_source = flx.Label(text='PanelSource')
 					self.pnl_tx = flx.Label(text='PanelTx', flex=1)
-		self.pnl_rx = None
+			self.lbl_status = flx.Label(text='...')
+		self.pnl_rx = flx.Label(text='PanelRx')
+		self.pnl_rx.set_parent(None)
 
 	def show_rx(self):
 		self.pnl_main.set_parent(None)  # Detach
-		if self.pnl_rx == None:
-			with self.root:
-				self.pnl_rx = flx.Label(text='PanelRx')
 		self.pnl_rx.set_parent(self.root)  # Attach
-		print('success!')
+
+	def show_tx(self, name, pkt):
+		self.pnl_main.set_parent(self.root)
+		self.pnl_rx.set_parent(None)
+		self.set_status(name)
+		self.pnl_tx.set_text(repr(pkt))
+
+	def set_status(self, status):
+		self.lbl_status.set_text(status)
 
 m = flx.launch(ScapyUI)
 flx.run()
