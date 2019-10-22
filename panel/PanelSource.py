dc6e5d8a3c642f6dd49fd9f563851b3e69380df4
Xueming Li <xuemingl@mellanox.com>
2019-10-22 08:01:05 +0800

    source: align layout to tx panel
    Signed-off-by: Xueming Li <xuemingl@mellanox.com>

--- panel/PanelSource.py
+++ panel/PanelSource.py
@@ -5,21 +5,24 @@ import os
 
 class PanelSource(flx.PyWidget):
 	def init(self):
-		with flx.HBox() as pnl_source:
-			flx.Label(text="Name: ", css_class="title",)
-			self.txt_name = flx.LineEdit(text="test", flex=1, )
-			self.btn_save = flx.Button(text='Apply')
-			self.btn_new = flx.Label(text='New', css_class="link",)
-			self.btn_del = flx.Label(text='Delete', css_class="link")
+		with flx.HFix():
+			flx.Label(text="Name: ", css_class="title", flex=2)
+			self.txt_name = flx.LineEdit(flex=16)
+			self.btn_save = flx.Button(text='Apply', flex=2)
+			self.btn_new = flx.Button(text='New', flex=2)
+			self.btn_del = flx.Button(text='Delete', flex=2)
 
 	@flx.reaction('btn_save.pointer_click')
 	def on_save(self, *events):
-		if len(self.txt_name):
-			self.root.save_config(self.txt_name.text)
+		txt = self.txt_name.text.strip()
+		if len(txt):
+			self.root.save_config(txt)
 
 	@flx.reaction('btn_del.pointer_click')
 	def on_del(self, *events):
-		self.root.del_config(self.txt_name.text)
+		txt = self.txt_name.text.strip()
+		if len(txt):
+			self.root.del_config(txt)
 
 	@flx.reaction('btn_new.pointer_click')
 	def on_new(self, *events):
