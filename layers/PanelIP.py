ec52f242ada1f4420f09a0c4c753228d3ef14386
Xueming Li <xuemingl@mellanox.com>
2019-10-22 12:13:53 +0800

    IP: use property to sync layer and detail panel
    Signed-off-by: Xueming Li <xuemingl@mellanox.com>

--- layers/PanelIP.py
+++ layers/PanelIP.py
@@ -15,58 +15,64 @@ def pkt_set(txt, pkt, name):
 
 
 class LayerIP(flx.PyWidget):
+	src = flx.StringProp("1", settable=True)
+	dst = flx.StringProp("2", settable=True)
+	scapy = flx.StringProp(settable=True)
+	hex = flx.StringProp(settable=True)
 	def init(self):
+		self.pkt_load(IP(src='192.168.1.1'))
 		with flx.HBox():
-			self.pkt = IP(src="1.2.3.4")
 			flx.Label(text="Src")
-			self.txt_src = flx.LineEdit()
+			self.txt_src = flx.LineEdit(text=lambda: self.src)
 			flx.Label(text="Dst")
-			self.txt_dst = flx.LineEdit()
+			self.txt_dst = flx.LineEdit(text=lambda: self.dst)
 			self.btn_detail = flx.Button(text="...")
-		self.refresh_pkt()
 		
 	@flx.reaction('btn_detail.pointer_click')
 	def on_detail(self, *events):
-		pkt_set(self.txt_src, self.pkt, 'src')
-		pkt_set(self.txt_dst, self.pkt, 'dst')
 		with self.root:
 		    pnl = PanelIP(self)
 		self.root.show_panel(pnl)
 	
-	def refresh_pkt(self):
-		txt_set(self.txt_src, self.pkt, 'src')
-		txt_set(self.txt_dst, self.pkt, 'dst')
+	@flx.reaction('txt_src.text', 'txt_dst.text')
+	def on_update(self, *events):
+		self.set_src(self.txt_src.text.strip())
+		self.set_dst(self.txt_dst.text.strip())
+		self.pkt_update()
+
+	def pkt_load(self, pkt):
+		self.pkt = pkt
+		self.set_src(self.pkt.src)
+		self.set_dst(self.pkt.dst)
+	
+	def pkt_update(self):
+		self.pkt.src = self.src
+		self.pkt.dst = self.dst
+		self.set_scapy(repr(self.pkt))
+		self.set_hex(hexdump(self.pkt, dump=True))
 
 class PanelIP(flx.PyWidget):
 	def init(self, parent):
 		self._parent = parent
-		self.pkt = parent.pkt
 		with flx.VBox():
 			flx.Label(text = parent.pkt.__class__.__name__, css_class="title")
 			with flx.FormLayout(flex=2):
-				self.txt_src = flx.LineEdit(title="Source")
-				txt_set(self.txt_src, self.pkt, 'src')
-				self.txt_dst = flx.LineEdit(title="Destination")
-				txt_set(self.txt_dst, self.pkt, 'dst')
+				self.txt_src = flx.LineEdit(title="Source", text=lambda: self._parent.src)
+				self.txt_dst = flx.LineEdit(title="Destination", text=lambda: self._parent.dst)
 			flx.Label()
-			flx.Label(text = "Scapy:", css_class="title")
-			self.lbl_scapy = flx.Label()
+			self.lbl_scapy = flx.Label(text=lambda: self._parent.scapy)
 			flx.Label()
-			flx.Label(text = "Hex:", css_class="title")
-			self.lbl_hex = flx.Label()
+			self.lbl_hex = flx.Label(text=lambda: self._parent.hex)
 			flx.Label()
 			self.btn_save = flx.Button(title="", text='Back')
 
 	@flx.reaction('btn_save.pointer_click')
 	def on_save(self, *events):
 		self.root.show_tx()
-		self._parent.refresh_pkt()
 
-	@flx.reaction
+	@flx.reaction('txt_src.text', 'txt_dst.text')
 	def on_update(self, *events):
-		# HOWTO bind src property?
-		# HOWTO bind src property?
-		pkt_set(self.txt_src, self.pkt, 'src')
-		pkt_set(self.txt_dst, self.pkt, 'dst')
-		self.lbl_scapy.set_text(repr(self.pkt))
-		self.lbl_hex.set_text(hexdump(self.pkt, dump=True))
+		self._parent.set_src(self.txt_src.text.strip())
+		self._parent.set_dst(self.txt_dst.text.strip())
+		self._parent.pkt_update()
+		
