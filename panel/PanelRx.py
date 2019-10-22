e2b1f6e69a27d950c3a8af411f83f21aa7982db2
Xiaoyu Min <jackmin@mellanox.com>
2019-10-22 17:15:10 +0800

    RX: show hexdump and detail info

--- panel/PanelRx.py
+++ panel/PanelRx.py
@@ -1,49 +1,29 @@
 from flexx import flx, ui, event
-import psutil
 from scapy.all import AsyncSniffer
-
+from scapy.utils import hexdump
 from time import time
+
 import psutil
 import asyncio
+import psutil
 
-from flexx import flx
 
 p_list = []
 ifname_list = []
 
-class SummaryBox(flx.Label):
+class ListLabel(flx.PyWidget):
+    idx = 0
+    def init(self, text, idx):
+        self.label = flx.Label(text = text, flex = 1, css_class="item")
+        self.idx = idx
 
-    CSS = """
-    .flx-SummaryBox {
-        overflow-x:scroll;
-        overflow-y:scroll;
-        background: #e8e8e8;
-        border: 1px solid #444;
-        margin: 3px;
-    }
-    """
+    @flx.reaction('label.pointer_click')
+    def on_click(self, *events):
+        relay.pkt_detail(self.idx)
+        relay.pkt_hexdump(self.idx)
 
-    def init(self):
-        super().init()
-        global window
-        self._se = window.document.createElement('div')
-
-    def sanitize(self, text):
-        self._se.textContent = text
-        text = self._se.innerHTML
-        self._se.textContent = ''
-        return text
-
-    @flx.action
-    def add_summary(self, msg):
-        #line =  '<pre>' + self.sanitize(msg) + '</ pre>'
-        line = '<br />'.join(msg.splitlines())
-        self.set_html(self.html + line + '<br />')
-
-    @flx.action
-    def clear_summary(self):
-        self.set_html('')
-
+    def dispose(self):
+        self.label.dispose()
 
 class Relay(flx.Component):
 
@@ -51,7 +31,7 @@ class Relay(flx.Component):
     prev_idx = 0
     curr_idx = 0
     summary_txt = ''
-    repr_txt = ''
+    detail_txt = ''
     hexdump_txt = ''
 
     def init(self):
@@ -60,7 +40,12 @@ class Relay(flx.Component):
         self.refresh()
 
     def sniff_start(self, ifname):
-        p_list.clear()
+        p_list[:] = []
+        self.summary_txt = ''
+        self.detail_txt = ''
+        self.hexdump_txt = ''
+        self.prev_idx = 0
+        self.curr_idx = 0
         self.sniffer = AsyncSniffer(iface=ifname, prn=lambda x:p_list.append(x))
         self.sniffer.start()
 
@@ -70,7 +55,6 @@ class Relay(flx.Component):
     def print_packet(self):
         self.curr_idx = len(p_list)
         self.summary_txt = ''
-        self.repr_txt = ''
         for i in range(self.prev_idx, self.curr_idx):
             self.summary_txt += p_list[i].summary()
             if i is not self.curr_idx - 1:
@@ -78,13 +62,27 @@ class Relay(flx.Component):
         self.prev_idx = self.curr_idx
         return self.summary_txt
 
+    def pkt_detail(self, idx):
+        if p_list[idx]:
+            self.detail_txt = p_list[idx].__repr__()
+        else:
+            self.detail_txt = ''
+
+    def pkt_hexdump(self, idx):
+        if p_list[idx]:
+            self.hexdump_txt = hexdump(p_list[idx], dump=True)
+        else:
+            self.hexdump_txt = ''
+
     @flx.emitter
     def system_info(self):
-        return dict(packets=self.print_packet())
+        return dict(packets=self.print_packet(),
+                    hexdump_txt=self.hexdump_txt,
+                    detail_txt=self.detail_txt)
 
     def refresh(self):
         self.system_info()
-        asyncio.get_event_loop().call_later(1, self.refresh)
+        asyncio.get_event_loop().call_later(0.5, self.refresh)
 
 
 # Create global relay
@@ -93,7 +91,7 @@ relay = Relay()
 class PanelRx(flx.PyWidget):
 
     def init(self):
-        with flx.VBox(css_class="debug"):
+        with flx.VBox():
             self.root.set_status('Sniffing')
             with flx.HBox():
                 self.iface=flx.ComboBox(options=relay.ifname, flex=2)
@@ -105,7 +103,6 @@ class PanelRx(flx.PyWidget):
     def update_iface(self):
         if self.iface.selected_index is not None:
             self.ifname = self.iface.text
-            print(self.ifname)
 
     @event.reaction('start_stop.pointer_click')
     def _start_stop_clicked(self, *events):
@@ -122,26 +119,70 @@ class PanelRx(flx.PyWidget):
         if not self.session.status:
             return relay.disconnect('system_info:' + self.id)
         for ev in events:
-            self.view.update_info(dict(packets=ev.packets))
+            self.view.update_info(dict(packets=ev.packets,
+                                       hexdump_txt=ev.hexdump_txt,
+                                       detail_txt=ev.detail_txt))
 
     def load_pkts(self, pkts):
         for pkt in pkts:
             print(pkt.summary())
 
 class PanelRxView(flx.PyWidget):
+    CSS = """
+        .detail {boarder: solid green 3px; background:white;}
+    """
+
+    labels = []
+    label_idx = 0
 
     def init(self):
-        with flx.VBox():
-            self.summary = SummaryBox(flex=1)
+        with flx.HSplit():
+            with flx.VBox(flex=1):
+                self.summary = flx.GroupWidget(title="Received Packets", flex=1, css_class="list")
+            with flx.VBox(flex=1):
+                with flx.GroupWidget(css_class="list", flex=1, title="Detail"):
+                    self.detail = flx.Label(flex=1)
+                    self.detail.set_wrap(2)
+                with flx.GroupWidget(css_class="list", flex=1, title="hexdump"):
+                    self.hexdump = flx.Label(flex=1)
+                    self.hexdump.set_wrap(2)
+                    self.hexdump.set_css_class("detail")
 
-    @flx.action
     def update_info(self, info):
-        if info['packets'] :
-            self.summary.add_summary(info['packets'])
+        if info['packets']:
+            with self.summary:
+                self.add_labels(info['packets'])
+        if info['hexdump_txt']:
+            line = '<pre><code>' + info['hexdump_txt'] + '</ code></ pre>'
+            self.hexdump.set_html(line)
+        if info['detail_txt']:
+            self.detail.set_text(info['detail_txt'])
 
-    @flx.action
+    def add_labels(self, msg):
+        for l in msg.splitlines():
+              self.add_one_label(l)
+
+    def add_one_label(self, msg):
+        self.labels.append(ListLabel(msg, self.label_idx))
+        self.label_idx += 1
+
+    def clear_labels(self):
+        for l in self.labels:
+            l.dispose()
+        self.labels.clear()
+        self.label_idx = 0
+        self.hexdump.set_text('')
+        self.detail.set_text('')
+
     def clear_info(self):
-        self.summary.clear_summary()
+        #self.summary.clear_summary()
+        self.clear_labels()
+
+    def show_detail(self, msg):
+        self.detail.set_text(msg)
+
+    def show_hexdump(self, msg):
+        self.hexdump.set_text(msg)
 
 if __name__ == '__main__':
     a = flx.App(PanelRx)
