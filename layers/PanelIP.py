95d574413935d08a6b5d04a41a56f33b853fc5b3
Xueming Li <xuemingl@mellanox.com>
2019-10-22 12:45:56 +0800

    layer/ip: update bindings demo
    Signed-off-by: Xueming Li <xuemingl@mellanox.com>

--- layers/PanelIP.py
+++ layers/PanelIP.py
@@ -3,20 +3,10 @@ from flexx import flx
 import configparser
 import os
 
-def txt_set(txt, pkt, name):
-	txt.set_text(pkt.fields.get(name, ""))
-		
-def pkt_set(txt, pkt, name):
-	if len(txt.text):
-		# pkt.fields[name] = txt.text
-		pkt.setfieldval(name, txt.text)
-	else:
-		pkt.fields.pop(name, None)
-
-
 class LayerIP(flx.PyWidget):
-	src = flx.StringProp("1", settable=True)
-	dst = flx.StringProp("2", settable=True)
+	src = flx.StringProp(settable=True)
+	dst = flx.StringProp(settable=True)
+	ttl = flx.StringProp(settable=True)
 	scapy = flx.StringProp(settable=True)
 	hex = flx.StringProp(settable=True)
 	def init(self):
@@ -42,37 +32,45 @@ class LayerIP(flx.PyWidget):
 
 	def pkt_load(self, pkt):
 		self.pkt = pkt
-		self.set_src(self.pkt.src)
-		self.set_dst(self.pkt.dst)
+		self.set_src(self.pkt.fields.get('src',""))
+		self.set_dst(self.pkt.fields.get('dst',""))
 	
 	def pkt_update(self):
 		self.pkt.src = self.src
 		self.pkt.dst = self.dst
-		self.set_scapy(repr(self.pkt))
+		if len(self.ttl):
+			self.pkt.ttl = int(self.ttl)
+		self.set_scapy(self.pkt.show(dump=True))
 		self.set_hex(hexdump(self.pkt, dump=True))
 
 class PanelIP(flx.PyWidget):
+	CSS = """
+		.flx-PanelIP {min-height: 600px;}
+	"""
+
 	def init(self, parent):
 		self._parent = parent
 		with flx.VBox():
-			flx.Label(text = parent.pkt.__class__.__name__, css_class="title")
-			with flx.FormLayout(flex=2):
+			flx.Label(text = parent.pkt.__class__.__name__, css_class="title", flex=1)
+			with flx.FormLayout(flex=10):
 				self.txt_src = flx.LineEdit(title="Source", text=lambda: self._parent.src)
 				self.txt_dst = flx.LineEdit(title="Destination", text=lambda: self._parent.dst)
-			flx.Label()
-			self.lbl_scapy = flx.Label(text=lambda: self._parent.scapy)
-			flx.Label()
-			self.lbl_hex = flx.Label(text=lambda: self._parent.hex)
-			flx.Label()
-			self.btn_save = flx.Button(title="", text='Back')
+				self.txt_ttl = flx.LineEdit(title="TTL", text=lambda: self._parent.ttl)
+			flx.Label(flex=1)
+			self.lbl_scapy = flx.MultiLineEdit(text=lambda: self._parent.scapy, flex=50)
+			flx.Label(flex=1)
+			self.lbl_hex = flx.Label(wrap=1, text=lambda: self._parent.hex, flex=5)
+			flx.Label(flex=1)
+			self.btn_save = flx.Button(title="", text='Back', flex=1)
 
 	@flx.reaction('btn_save.pointer_click')
 	def on_save(self, *events):
 		self.root.show_tx()
 
-	@flx.reaction('txt_src.text', 'txt_dst.text')
+	@flx.reaction('txt_src.text', 'txt_dst.text', 'txt_ttl.text')
 	def on_update(self, *events):
 		self._parent.set_src(self.txt_src.text.strip())
 		self._parent.set_dst(self.txt_dst.text.strip())
+		self._parent.set_ttl(self.txt_ttl.text.strip())
 		self._parent.pkt_update()
 		
