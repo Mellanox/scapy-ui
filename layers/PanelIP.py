963e58a2466ddd3b5876095392345839b41a1564
Xueming Li <xuemingl@mellanox.com>
2019-10-30 14:39:55 +0800

    IP: add IP flags hints
    Signed-off-by: Xueming Li <xuemingl@mellanox.com>

--- layers/PanelIP.py
+++ layers/PanelIP.py
@@ -6,10 +6,10 @@ from layers.LayerBasic import *
 ip_descs = {
     "src":IpDesc("Source"),
     "dst":IpDesc("Destination"),
-    "tos":FieldDesc("TOS", int),
+    "tos":FieldDesc("TOS", int, ("#DSCP:6 << 2 + ECN:2", "# https://en.wikipedia.org/wiki/Type_of_service"), url="https://en.wikipedia.org/wiki/Type_of_service"),
     "len":FieldDesc("Length", int),
     "ids":FieldDesc("ID", int),
-    "flags":FieldDesc("Flags", int),
+    "flags":FieldDesc("Flags", int, ("1 #Multiple Fragment", "2 #Don't Fragment")),
     "frag":FieldDesc("Fragment", int),
     "ttl":FieldDesc("TTL", int),
     "chksum":FieldDesc("Checksum", int)
